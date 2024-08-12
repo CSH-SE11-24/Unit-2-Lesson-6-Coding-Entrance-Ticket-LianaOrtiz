71e16171d4f662b2669cc04569a17092b23a61b8
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 14:27:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// In this entrance ticket, you will be building a "guess the number" game. 
+// Create a new variable and put a number from 1-100 in
+let number = 6;
+
+// Prompt the user to guess the number
+let user = prompt("Guess the number")
+
+// Cast the user input to a number
+user = parseInt(user)
+
+
+// Create a new variable called attempts and set it to 0
+let attempts = 0
+
+// Create a while loop that runs while the user guess is not equal to your number
+while (user != number) {
+  console.log("Thats not the number try again.")
+  user = prompt("Guess the number again")
+  user = parseInt(user)
+  attempts++
+  if (user == number) {
+    console.log("You got it!")
+  } else if (user > number) {
+    console.log("Too high")
+  } else if (user < number) {
+    console.log("Too low")
+  }
+}
+console.log("It took", attempts, "attempts")
+// In the while loop, prompt the user to guess the number again (remember to cast)
+
+
+// In the while loop, increase the attempts by 1
+
+
+// In the while loop, write an if statement that does the following
+// If the user guess is higher, say "Too high!"
+// If the user guess is lower, say "Too low!"
+// If the user guess is right, say "You got it!"
+
+
+
+
+// After the while loop, print "It took (attempts) attempts" 
+
